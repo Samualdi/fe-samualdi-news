5a27f041c014b3f9d09f1f6ac61405bde168572a
samualdi <samualdick89@gmail.com>
2021-10-19 08:51:32 +0100

    added SingleArticle component

---
+++ src/components/SingleArticle.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SingleArticle = () => {
+    return (
+        <div>
+            <h3> This shows a single article</h3>
+        </div>
+    );
+};
+
+export default SingleArticle;
